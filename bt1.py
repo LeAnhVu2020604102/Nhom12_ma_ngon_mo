13e4d856aa3ad63aeaa1aa13b1f02636d4022095
LeAnhVu2020604102 <144864698+LeAnhVu2020604102@users.noreply.github.com>
2023-10-25 21:43:43 +0700

    Update bt1.py

--- bt1.py
+++ bt1.py
@@ -1,5 +1,27 @@
 import cv2
 import numpy as np
+import tkinter as tk
+from tkinter import filedialog
+
+def apply_filter(filter_type):
+    global img, selected_filter
+    if img is not None:
+        if filter_type == "sharpen_1":
+            kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        elif filter_type == "sharpen_2":
+            kernel = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]])
+        elif filter_type == "sharpen_3":
+            kernel = np.array([[-1, -1, -1, -1, -1],
+                               [-1, 2, 2, 2, -1],
+                               [-1, 2, 8, 2, -1],
+                               [-1, 2, 2, 2, -1],
+                               [-1, -1, -1, -1, -1]]) / 8.0
+        else:
+            kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+
+        output = cv2.filter2D(img, -1, kernel)
+        cv2.imshow('Filtered Image', output)
+        selected_filter = filter_type
 img = cv2.imread('anh1.JPG')
 rows, cols = img.shape[:2]
 kernel_identity = np.array([[0,0,0], [0,1,0], [0,0,0]])
